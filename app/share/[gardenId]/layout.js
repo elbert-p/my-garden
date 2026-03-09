2dc218cc9acff7385686f34ce29cd19fce84d945
elbert-p <elbert.p@northeastern.edu>
2026-03-09 14:44:55 -0400

    wrong save color

--- app/share/[gardenId]/layout.js
+++ app/share/[gardenId]/layout.js
@@ -88,7 +88,7 @@ function SharedGardenLayoutContent({ children }) {
   ];
 
   const menuItems = [
-    { icon: <FiBookmark size={16} fill={isSaved ? '#FFC107' : 'none'} color={isSaved ? '#b06000' : 'currentColor'} />, 
+    { icon: <FiBookmark size={16} fill={isSaved ? '#FFC107' : 'none'} color={isSaved ? '#FFC107' : 'currentColor'} />, 
       label: isSaved ? 'Unsave' : 'Save', onClick: handleToggleSave, variant: 'save' },
     { icon: <FiCopy size={16} />, label: 'Make a copy', onClick: handleCopyGarden },
   ];
